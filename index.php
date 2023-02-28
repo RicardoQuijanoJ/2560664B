d0af7da24e4e0d9adede3a55dfef83ad5c841da2
RicardoQuijanoJ <jricardo.quijanoj@gmail.com>
2023-02-28 16:07:38 -0500

    FrontEnd

---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="estilos/estilos.css">
+    <title>Programación Python</title>
+</head>
+<body>
+    <header>
+        <div class="contenedor_izq"><img class="img1" src="imagenes/python.png" alt="img izquierda"></div>
+        <div class="contenedor_cent"><strong>PROGRAMANDO EN PYTHON</strong></div>
+        <div class="contenedor_der"><img class="img1" src="imagenes/python2.png" alt="img derecha"></div>
+    </header>
+    <nav></nav>
+    <section></section>
+    <aside></aside>
+    <footer></footer>
+    
+</body>
+</html>
